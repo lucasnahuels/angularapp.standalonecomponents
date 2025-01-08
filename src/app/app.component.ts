4a30e832c7388337969196e5bc72017f4c85db0d
lucasnahuels <lucasnahuelstp@gmail.com>
2025-01-08 04:04:18 -0300

    work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { PersonListComponent } from './person-list/person-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, RouterModule, CommonModule],
+  imports: [RouterOutlet, RouterModule, CommonModule, PersonListComponent],
 })
 export class AppComponent {
   title = 'angularapp1.client';
