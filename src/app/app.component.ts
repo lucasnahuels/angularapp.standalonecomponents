a37aa0fe95eba07b12b61a2ac4a8d6e1fa4ff35b
lucasnahuels <lucasnahuelstp@gmail.com>
2025-01-08 03:34:06 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, RouterModule, CommonModule],
+})
+export class AppComponent {
+  title = 'angularapp1.client';
+}
